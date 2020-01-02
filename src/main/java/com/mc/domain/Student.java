964f38af9a1e60e91633dc60cfa34db64bae2ce6
dell <dell@DESKTOP-3V3NUGO>
2020-01-02 20:22:02 +0800

    斤斤计较

--- src/main/java/com/mc/domain/Student.java
+++ src/main/java/com/mc/domain/Student.java
@@ -5,7 +5,7 @@ public class Student {
 	public static void main(String[] args) {
 		
 		System.out.println("helloWord");
-		System.out.println("闫东浩大傻逼");
+		System.out.println("斤斤计较军军不");
 		System.out.println("马东浩");
 	}
 }
